a4aeaebcdfb92df11c399f20530c1546168225c5
Rahilkazi <rahilkazi66@gmail.com>
2023-12-07 05:24:57 +0530

    publish

---
+++ api/apib.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, jsonify
+from bardapi import Bard
+import requests
+
+
+BARD_API_KEY = 'dAga7-P7PY1zNCG2YaQZqKC3IIbvfiH20kSWd9Cj7bvHPZSvvFi1nej8njEBUGmgln0IRA.'
+
+session = requests.Session()
+session.headers = {
+    "Host": "bard.google.com",
+    "X-Same-Domain": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+    "Origin": "https://bard.google.com",
+    "Referer": "https://bard.google.com/",
+}
+session.cookies.set("__Secure-1PSID", BARD_API_KEY)
+
+bard = Bard(token=BARD_API_KEY, session=session, timeout=30)
+
+# Initial conversation
+
+# personality = bard.get_answer("")['content']
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def get_response():
+    try:
+        user_input = request.args.get('user_input', '')
+        response = process_input(user_input)
+        return (response)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+
+# Start interactive loop
+
+def process_input(user_input):
+    # Continued conversation without setting a new session
+    response = bard.get_answer(user_input)
+    # print(f"Bard: {response['content']}")
+    print("++++++++++++++++++++")
+    # Display images if any
+    result = {'content': response['content']}
+    
+    if 'images' in response:
+        result['images'] = response['images']
+
+    if 'links' in response:
+        result['links'] = response['links']
+
+    return result
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
